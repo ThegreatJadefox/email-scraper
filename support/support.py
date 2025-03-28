e6fa805b1ed70a193f2fc0bd16f7f5325c3d03a4
ThegreatJadefox <107548302+ThegreatJadefox@users.noreply.github.com>
2025-03-28 14:02:29 +0100

    Update support.py

--- support/support.py
+++ support/support.py
@@ -3,7 +3,7 @@ import streamlit as st
 def support_me():
     st.header("Thanks in advance for your support!")
     st.write("Your support would motivate me to add more features to this project. 😊")
-    col1, col2, col3 = st.columns(3, gap = "small", vertical_alignment = "center")
+    col1, col2, col3, col4 = st.columns(4, gap = "small", vertical_alignment = "center")
     with col1:
         st.subheader("Opay")
         st.write("7019567938\n\nAkintola Oladapo Julius\n")
@@ -13,6 +13,8 @@ def support_me():
     with col3:
         st.subheader("Crypto Transfer")
         st.write("BTC wallet address\n1Dcpo8oWa6QwA5wVYER4UbWZh4zKWE3C85")
+    with col4:
+        st.subheader("USDT")
         st.write("USDT\n TRC20\nTSgoS6vPmfbtTzL7VAxSmr5ZShYHS3Rveq")
 
     st.write("Email verification features coming soon ...")
