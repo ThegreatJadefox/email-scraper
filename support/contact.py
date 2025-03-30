fc649c913182ba138d39bc840f2a4dfa4af3c2b2
Oladapo <107548302+ThegreatJadefox@users.noreply.github.com>
2025-03-30 10:49:42 +0100

    Update contact.py

--- support/contact.py
+++ support/contact.py
@@ -7,7 +7,7 @@ def is_valid_email(email):
     email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(email_pattern, email) is not None
 
-def contact_form():
+def contact_me():
     st.title("Contact Us")
     
     with st.form("Contact Form"):
